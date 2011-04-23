b8d775fdb592b183e1841c739c29d1cf703b60a0
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2011-04-23 10:05:42 -0500

    Remove dups in tags

--- bkmarks.js
+++ bkmarks.js
@@ -232,7 +232,7 @@ $(function() {
     },
 
     parseTags: function(tags_input) {
-      return tags_input.split(/,?\s\s*/);
+      return _.uniq(tags_input.split(/,?\s\s*/));
     },
 
     clear: function() {
